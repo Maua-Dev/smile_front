f76435b6beeffcfdcd5c91d82a3b5fd38858f120
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2021-12-07 13:17:16 -0300

    Estruturação home page 1

--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,16 +1,12 @@
 import 'package:flutter_modular/flutter_modular.dart';
-
-import 'home_controller.dart';
-import 'home_page.dart';
+import 'package:smile_front/app/modules/home/ui/pages/home1/home1_page.dart';
 
 class HomeModule extends Module {
   @override
-  final List<Bind> binds = [
-    Bind.lazySingleton((i) => HomeController()),
-  ];
+  final List<Bind> binds = [];
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute(Modular.initialRoute, child: (_, args) => const HomePage()),
+    ChildRoute(Modular.initialRoute, child: (_, args) => const Home1Page()),
   ];
 }
---
+++ lib/app/modules/home/ui/pages/home1/home1_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home1Page extends StatefulWidget {
+  const Home1Page({Key? key}) : super(key: key);
+
+  @override
+  _HomePage1State createState() => _HomePage1State();
+}
+
+class _HomePage1State extends State<Home1Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
