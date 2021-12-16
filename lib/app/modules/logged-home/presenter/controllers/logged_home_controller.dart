c69b7baa9e3536e32157a7409c762cac10ea64b3
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2021-12-16 10:56:50 -0300

    Criação do controller para logged home

---
+++ lib/app/modules/logged-home/presenter/controllers/logged_home_controller.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+
+part 'logged_home_controller.g.dart';
+
+class LoggedHomeController = _LoggedHomeControllerBase
+    with _$LoggedHomeController;
+
+abstract class _LoggedHomeControllerBase with Store {}
