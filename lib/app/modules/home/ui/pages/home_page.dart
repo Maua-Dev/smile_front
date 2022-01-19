9249bb11d87359abfbca6a5c1617f71419b80b8d
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-01-19 08:19:40 -0300

    Update home_page.dart

--- lib/app/modules/home/ui/pages/home_page.dart
+++ lib/app/modules/home/ui/pages/home_page.dart
@@ -85,7 +85,6 @@ class _HomePageState extends State<HomePage> {
                 onPressed: () async {
                   await Modular.isModuleReady<AppModule>();
                   Modular.to.navigate('/login');
-
                 },
               ),
             )
