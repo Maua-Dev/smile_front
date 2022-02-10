e473af00301b8d5b85223ce51b2c56c194a7e2ad
FePaganini <felipespaganini@hotmail.com>
2022-02-10 19:56:02 -0300

    home 4 quase finalizada

--- lib/app/modules/home/ui/pages/home4/home4_page.dart
+++ lib/app/modules/home/ui/pages/home4/home4_page.dart
@@ -11,29 +11,119 @@ class Home4Page extends StatefulWidget {
   _Home4PageState createState() => _Home4PageState();
 }
 
-double imageSize(size) {
+int crossAxisCount(size, height) {
+  if (size <= 1920 && size >= 1760) {
+    if (height <= 1080 && height >= 990) return 3;
+    if (height <= 990 && height >= 900) return 3;
+    if (height <= 900 && height >= 810) return 4;
+    if (height <= 810 && height >= 720) return 4;
+    if (height <= 720 && height >= 630) return 4;
+    return 4;
+  }
+  if (size <= 1760 && size >= 1600) {
+    if (height <= 1080 && height >= 990) return 3;
+    if (height <= 990 && height >= 900) return 3;
+    if (height <= 900 && height >= 810) return 4;
+    if (height <= 810 && height >= 720) return 4;
+    if (height <= 720 && height >= 630) return 4;
+    return 4;
+  }
+  if (size <= 1600 && size >= 1440) {
+    if (height <= 1080 && height >= 990) return 3;
+    if (height <= 990 && height >= 900) return 3;
+    if (height <= 900 && height >= 810) return 4;
+    if (height <= 810 && height >= 720) return 4;
+    if (height <= 720 && height >= 630) return 4;
+    return 4;
+  }
+  if (size <= 1440 && size >= 1280) {
+    if (height <= 1080 && height >= 990) return 3;
+    if (height <= 990 && height >= 900) return 3;
+    if (height <= 900 && height >= 810) return 4;
+    if (height <= 810 && height >= 720) return 4;
+    if (height <= 720 && height >= 630) return 4;
+    return 4;
+  }
+  if (size <= 1280 && size >= 1120) {
+    if (height <= 1080 && height >= 990) return 3;
+    if (height <= 990 && height >= 900) return 3;
+    if (height <= 900 && height >= 810) return 4;
+    if (height <= 810 && height >= 720) return 4;
+    if (height <= 720 && height >= 630) return 4;
+    return 4;
+  }
+  if (size <= 1280 && size >= 1120) {
+    if (height <= 1080 && height >= 990) return 3;
+    if (height <= 990 && height >= 900) return 3;
+    if (height <= 900 && height >= 810) return 4;
+    if (height <= 810 && height >= 720) return 4;
+    if (height <= 720 && height >= 630) return 4;
+    return 4;
+  }
+  if (size <= 1120 && size >= 960) {
+    if (height <= 1080 && height >= 990) return 3;
+    if (height <= 990 && height >= 900) return 3;
+    if (height <= 900 && height >= 810) return 4;
+    if (height <= 810 && height >= 720) return 4;
+    if (height <= 720 && height >= 630) return 4;
+    return 4;
+  }
+  return 4;
+}
+
+double imageSize(size, height) {
+  if (size < 1920 && size >= 1760) {
+    if (height <= 1080 && height >= 990) return 220;
+    if (height <= 990 && height >= 900) return 190;
+    if (height <= 900 && height >= 810) return 250;
+    if (height <= 810 && height >= 720) return 210;
+    if (height <= 720 && height >= 630) return 160;
+    return 200;
+  }
+  if (size < 1760 && size >= 1600) {
+    if (height <= 1080 && height >= 990) return 220;
+    if (height <= 990 && height >= 900) return 190;
+    if (height <= 900 && height >= 810) return 250;
+    if (height <= 810 && height >= 720) return 210;
+    if (height <= 720 && height >= 630) return 160;
+    return 200;
+  }
   if (size < 1600 && size >= 1440) {
+    if (height <= 1080 && height >= 990) return 220;
+    if (height <= 990 && height >= 900) return 190;
+    if (height <= 900 && height >= 810) return 260;
+    if (height <= 810 && height >= 720) return 210;
+    if (height <= 720 && height >= 630) return 160;
     return 200;
   }
   if (size < 1440 && size >= 1280) {
+    if (height <= 1080 && height >= 990) return 220;
+    if (height <= 990 && height >= 900) return 190;
+    if (height <= 900 && height >= 810) return 250;
+    if (height <= 810 && height >= 720) return 210;
+    if (height <= 720 && height >= 630) return 160;
     return 200;
   }
-  if (size < 1280 && size >= 960) {
+  if (size < 1280 && size >= 1120) {
+    if (height <= 1080 && height >= 990) return 220;
+    if (height <= 990 && height >= 900) return 190;
+    if (height <= 900 && height >= 810) return 270;
+    if (height <= 810 && height >= 720) return 210;
+    if (height <= 720 && height >= 630) return 160;
+    return 200;
+  }
+  if (size < 1120 && size >= 960) {
+    if (height <= 1080 && height >= 990) return 220;
+    if (height <= 990 && height >= 900) return 190;
+    if (height <= 900 && height >= 810) return 270;
+    if (height <= 810 && height >= 720) return 220;
+    if (height <= 720 && height >= 630) return 160;
     return 200;
   }
   return 200;
 }
 
-double imagePadding(size) {
-  if (size < 1600 && size >= 1440) {
-    return 200;
-  }
-  if (size < 1440 && size >= 1280) {
-    return 150;
-  }
-  if (size < 1280 && size >= 960) {
-    return 100;
-  }
+double imagePadding(size, height) {
   return 100;
 }
 
@@ -41,6 +131,7 @@ class _Home4PageState extends ModularState<Home4Page, Home4Controller> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     return Padding(
       padding: const EdgeInsets.only(top: 16),
       child: Column(
@@ -50,12 +141,12 @@ class _Home4PageState extends ModularState<Home4Page, Home4Controller> {
             child: Observer(builder: (_) {
               return Padding(
                 padding: EdgeInsets.symmetric(
-                    vertical: 48, horizontal: imagePadding(size)),
+                    vertical: 48, horizontal: imagePadding(size, height)),
                 child: GridView.builder(
                   physics: const NeverScrollableScrollPhysics(),
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      mainAxisExtent: imageSize(size),
-                      crossAxisCount: 4,
+                      mainAxisExtent: imageSize(size, height),
+                      crossAxisCount: crossAxisCount(size, height),
                       mainAxisSpacing: 16,
                       crossAxisSpacing: 32),
                   itemCount: controller.listLectureImages.length,
