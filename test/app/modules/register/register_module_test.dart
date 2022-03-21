f82569e8d222efe9124937ec0c998994c3d4fcdf
Caio Toledo <caio.detoledo15@gmail.com>
2022-03-21 12:22:38 -0300

    retirando importação não utilizada

--- test/app/modules/register/register_module_test.dart
+++ test/app/modules/register/register_module_test.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_modular_test/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:smile_front/app/app_module.dart';
 import 'package:smile_front/app/modules/register/domain/repositories/register_informations_repository_interface.dart';
 import 'package:smile_front/app/modules/register/infra/datasources/register_datasource.dart';
 import 'package:smile_front/app/modules/register/presenter/controllers/register_controller.dart';
