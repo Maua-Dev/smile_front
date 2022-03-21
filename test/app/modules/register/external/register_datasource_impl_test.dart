3120274e55074d2a6a8f0417cc7b67023b82f3a0
Caio Toledo <caio.detoledo15@gmail.com>
2022-03-21 12:19:55 -0300

    testes unitários para o modulo de cadastro

---
+++ test/app/modules/register/external/register_datasource_impl_test.dart
@@ -0,0 +1,18 @@
+import 'package:dio/native_imp.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:smile_front/app/modules/register/external/register_datasource_impl.dart';
+import 'package:smile_front/app/shared/entities/user_registration.dart';
+import 'package:smile_front/app/shared/services/dio/smile_login_options.dart';
+
+void main() {
+  test('registerUser', () async {
+    final ds =
+        RegisterDatasourceImpl(dioClient: DioForNative(smileLoginOption));
+    final res = await ds.registerUser(UserRegistration(
+        name: 'Caio Toledo',
+        email: 'caio@caio.com',
+        cpfRne: '63480154085',
+        password: 'Teste123!'));
+    expect(res.isNotEmpty, true);
+  });
+}
