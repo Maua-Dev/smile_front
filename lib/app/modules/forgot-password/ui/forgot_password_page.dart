9fca365ceb7cbfc3aa224dad795795facef1710a
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-03-30 18:46:29 -0300

    Remove unused imports

--- lib/app/modules/forgot-password/ui/forgot_password_page.dart
+++ lib/app/modules/forgot-password/ui/forgot_password_page.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
