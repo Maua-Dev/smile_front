d1a5bada1f11a416dc9401c98232bfc3993639cc
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-03-30 16:14:25 -0300

    Folder adjust

--- lib/app/modules/dashboard/external/activities_datasource_impl.dart
+++ lib/app/modules/dashboard/external/activities_datasource_impl.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:smile_front/app/modules/dashboard/infra/datasources/activities_datasource.dart';
 import 'package:smile_front/app/shared/models/activity_model.dart';
 
-import '../utils/subscribed_activities_mock.dart';
+import '../utils/mocks/subscribed_activities_mock.dart';
 
 class ActivitiesDatasourceImpl extends ActivitiesDatasource {
   final Dio dioClient;
