6656e4770c21f71e5b506054065389ee5b233955
CAIO BARTOLOZZI BASTOS GODOY DE TOLEDO <20.01430-9@maua.br>
2022-04-04 16:53:16 -0300

    adicionando valores das variáveis com default

--- lib/app/shared/services/enviroment/enviroment_config.dart
+++ lib/app/shared/services/enviroment/enviroment_config.dart
@@ -1,8 +1,14 @@
 // ignore_for_file: constant_identifier_names
 
 class EnvironmentConfig {
-  static const MSS_USER_BASE_URL = String.fromEnvironment('MSS_USER_BASE_URL');
-  static const MSS_ACTIVITIES_BASE_URL =
-      String.fromEnvironment('MSS_ACTIVITIES_BASE_URL');
-  static const S3_BASE_URL = String.fromEnvironment('S3_BASE_URL');
+  static const MSS_USER_BASE_URL = String.fromEnvironment('MSS_USER_BASE_URL',
+      defaultValue:
+          'https://76jdrw8lbe.execute-api.us-east-1.amazonaws.com/dev/smile-mss-cognito');
+  static const MSS_ACTIVITIES_BASE_URL = String.fromEnvironment(
+      'MSS_ACTIVITIES_BASE_URL',
+      defaultValue:
+          'http://lb-1268416342.us-east-1.elb.amazonaws.com/smile_mss_activities');
+  static const S3_BASE_URL = String.fromEnvironment('S3_BASE_URL',
+      defaultValue:
+          'https://smile2022-frontend-assets.s3.sa-east-1.amazonaws.com/');
 }
