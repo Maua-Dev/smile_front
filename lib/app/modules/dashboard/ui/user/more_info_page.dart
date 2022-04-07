ff516d6c6d3803a5d211ba985b1733ebf7010a68
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-04-07 13:31:15 -0300

    Rename of dialog widget

--- lib/app/modules/dashboard/ui/user/more_info_page.dart
+++ lib/app/modules/dashboard/ui/user/more_info_page.dart
@@ -6,7 +6,7 @@ import 'dart:math' as math;
 import 'package:smile_front/app/modules/dashboard/domain/infra/activity_enum.dart';
 import 'package:smile_front/app/shared/entities/card_activity.dart';
 import 'package:smile_front/app/shared/themes/app_colors.dart';
-import 'package:smile_front/app/shared/widgets/dialogs/nothing_to_see_dialog_widget.dart';
+import 'package:smile_front/app/shared/widgets/dialogs/custom_alert_dialog_widget.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../../../shared/themes/app_text_styles.dart';
 import '../../../../shared/utils/utils.dart';
