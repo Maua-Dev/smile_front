b3ac449b7087a61fd812e373d47f70f96d48270e
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-04-10 16:39:35 -0300

    Atualização das perguntas frequentes

--- lib/app/modules/dashboard/utils/mocks/faq_mock.dart
+++ lib/app/modules/dashboard/utils/mocks/faq_mock.dart
@@ -1,6 +1,38 @@
 import '../../domain/entities/faq.dart';
 
 var faqMock = <Faq>[
-  Faq(question: 'O que é GCSP?', response: 'Grand Challenges Scholars Program'),
-  Faq(question: 'O que é GCSP?', response: 'Grand Challenges Scholars Program'),
+  Faq(
+      question: 'Como validar a presença nas atividades?',
+      response:
+          'Será disponibilizado durante a atividade um Formulário para preenchimento do aluno que validará as suas horas na atividade.'),
+  Faq(
+      question: 'Como emitir o meu Certificado?',
+      response:
+          'O Certificado ainda não está disponível, aguarde mais informações.'),
+  Faq(
+      question: ' Onde vejo as atividades da SMILE?',
+      response:
+          'Clicando no ícone central da barra de navegação você poderá visualizar todas as atividades fornecidas pela Smile.'),
+  Faq(
+      question: 'Nas atividades presenciais, onde posso ver a sala?',
+      response:
+          'Ao clicar na atividade em que você se inscreveu, você poderá ver todas as informações disponíveis sobre aquela atividade, inclusive o local.'),
+  Faq(
+      question: 'Nas visitas técnicas posso ir de bermuda?',
+      response: 'É de extrema importancia ir de calça para sua segurança!'),
+  Faq(
+      question: 'Fiz o meu login e esqueci a senha. O que devo fazer?',
+      response:
+          'Existe uma opção na tela de "Login" chamada "Esqueci minha senha", onde você poderá alterá-la seguindo o passo-a-passo apresentado lá.'),
+  Faq(
+      question: 'A partir de quando posso emitir meu certificado?',
+      response: 'Novas Informações em breve!'),
+  Faq(
+      question:
+          'As gravações das atividades on-line estarão disponíveis? Onde e quando?',
+      response: 'Sim, mais informações em breve!'),
+  Faq(
+      question: 'Como cancelar minha inscrição em uma atividade?',
+      response:
+          'Ao clicar na atividade que você se inscreveu erroneamente, haverá um botão vermelho "Desinscrever", basta clicar nele e confirmar.'),
 ];
