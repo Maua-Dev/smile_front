2050345e591bc199d060b2bfaf33459423e28c14
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-04-18 15:27:34 -0300

    faq new questions

--- lib/app/modules/dashboard/utils/mocks/faq_mock.dart
+++ lib/app/modules/dashboard/utils/mocks/faq_mock.dart
@@ -2,41 +2,70 @@ import '../../domain/entities/faq.dart';
 
 var faqMock = <Faq>[
   Faq(
-      question: 'Como validar a presença nas atividades?',
-      response:
-          'Será disponibilizado durante a atividade um Formulário para preenchimento do aluno que validará as suas horas na atividade.'),
-  Faq(
-      question: 'Como alterar meu nome/nome social para a certificação?',
-      response:
-          'O nome ou nome social que você se registrou é o que será utilizado por padrão para gerar os certificados. Caso tenha inserido algo errado ou não deseje utilizar seu nome social na certificação, você poderá alterá-lo até dia 23/04. Basta ir até a página de perfil e clicar no icone de lápis ao lado de seu nome.'),
-  Faq(
-      question: 'Como emitir o meu Certificado?',
-      response:
-          'O Certificado ainda não está disponível, aguarde mais informações.'),
-  Faq(
-      question: ' Onde vejo as atividades da SMILE?',
-      response:
-          'Clicando no ícone central da barra de navegação você poderá visualizar todas as atividades fornecidas pela Smile.'),
-  Faq(
-      question: 'Nas atividades presenciais, onde posso ver a sala?',
-      response:
-          'Ao clicar na atividade em que você se inscreveu, você poderá ver todas as informações disponíveis sobre aquela atividade, inclusive o local.'),
-  Faq(
-      question: 'Nas visitas técnicas posso ir de bermuda?',
-      response: 'É de extrema importancia ir de calça para sua segurança!'),
-  Faq(
       question: 'Fiz o meu login e esqueci a senha. O que devo fazer?',
       response:
           'Existe uma opção na tela de "Login" chamada "Esqueci minha senha", onde você poderá alterá-la seguindo um passo-a-passo.'),
   Faq(
-      question: 'A partir de quando posso emitir meu certificado?',
-      response: 'Novas Informações em breve!'),
+      question: 'Como validar a presença nas minhas atividades?',
+      response:
+          'Será disponibilizado durante a atividade um Formulário para preenchimento do participante que validará as suas horas na atividade.'),
+  Faq(
+      question: 'Em que local no site eu vejo as atividades da SMILE?',
+      response:
+          'Clicando no ícone central da barra de navegação você poderá visualizar todas as atividades do evento. Se você for estudante do IMT poderá também acessar pelo Open LMS da SMILE: https://imt.myopenlms.net/course/view.php?id=1353'),
+  Faq(
+      question: 'Na atividade presencial, onde vejo a sala?',
+      response:
+          'Ao clicar na atividade em que você se inscreveu, você poderá ver todas as informações disponíveis sobre aquela atividade, inclusive a sala.'),
+  Faq(
+      question: 'Nas visitas técnicas posso estar vestido de bermuda?',
+      response:
+          'As Visitas Técnicas tem algumas orientações importantes! Assim ANTES de você fazer a sua inscrição LEIA com ATENÇÃO as informações necessárias que estão no site da SMILE. A recomendação padrão para você participar de uma visita é estar vestido de calça comprida e sapato de couro. Bermudas não serão permitidas.'),
   Faq(
       question:
           'As gravações das atividades on-line estarão disponíveis? Onde e quando?',
-      response: 'Sim, mais informações em breve!'),
+      response:
+          'As gravações estarão disponíveis a partir do dia 30.05. Se você for do IMT poderá fazer o download no ambiente Open LMS da SMILE e caso você seja um participante externo poderá solicitar pelo e-mail smile@maua.br'),
   Faq(
       question: 'Como cancelar minha inscrição em uma atividade?',
       response:
-          'Ao clicar na atividade que você se inscreveu erroneamente, haverá um botão vermelho "Desinscrever-se", basta clicar nele e confirmar.'),
+          'Ao clicar na atividade que você quer cancelar haverá um botão vermelho "Desinscrever-se", basta clicar nele e confirmar.'),
+  Faq(
+      question: 'Como realizo acesso ao Zoom para assistir à palestra?',
+      response:
+          'Ao clicar na atividade em que você se inscreveu haverá um campo "Link" sublinhado e clicável de "Zoom" que irá te redirecionar para a reunião.'),
+  Faq(
+      question: 'Inseri meu e-mail errado. Como pedir alteração?',
+      response:
+          'Envie um e-mail para smile@maua.br ou dev@maua.br ou ligue para 4239-3169.'),
+  Faq(
+      question: 'Como emitir o meu Certificado?',
+      response:
+          'O Certificado será emitido por você acessando o site da SMILE.'),
+  Faq(
+      question: 'Como emitir o meu Certificado?',
+      response:
+          'O Certificado será emitido por você acessando o site da SMILE.'),
+  Faq(
+      question: 'Como alterar meu nome/nome social para a certificação?',
+      response:
+          'Basta navegar até a página de perfil e clicar no icone de lápis ao lado de seu nome.'),
+  Faq(
+      question:
+          'Digitei meu nome com uma letra errada no meu Certificado. Até que data posso alterar?',
+      response:
+          'O nome incluído no sistema será o que utilizaremos para fins de emissão de certificado. Qualquer alteração no seu cadastro poderá ser feita até o dia 23.04, sob pena do certificado ser emitido com os dados fornecidos por você. Fique atento!'),
+  Faq(
+      question: 'A partir de quando posso emitir meu certificado?',
+      response:
+          'Os Certificados estarão disponíveis para download no site a partir de 30 de abril de 2022.'),
+  Faq(
+      question:
+          'Quais são os links nas redes sociais para compartilhar o evento com amigos?',
+      response:
+          'Visite o site da SMILE: smile.maua.br e você poderá encontrar toda a programação. Você também poderá acompanhar as informações pelo nosso Instagram @infomaua'),
+  Faq(
+      question: 'Em caso de dúvidas, como faço para entrar em contato?',
+      response:
+          'Enviar um e-mail para smile@maua.br ou gti.central@maua.br ou dev@maua.br. Caso queira falar consoco, entre em contato nos números: 4239-3002, 4239-3014, 4239-3441, 4239-3442 ou 4239-3411.'),
 ];
