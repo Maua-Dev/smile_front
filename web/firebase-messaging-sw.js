f64aac258e8b30cbd89719699b6d1f045d2257b8
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2022-05-08 23:22:50 -0300

    fix  sw

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -1,6 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.8.0/firebase-messaging.js");
-
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 firebase.initializeApp({
     apiKey: 'AIzaSyALQsK3VSz1_R9x8TJmw6uO7lwqRyhAJTs',
     appId: '1:272148621093:web:21e6054d9a5e31e7e95ccc',
