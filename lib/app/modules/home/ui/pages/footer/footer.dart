a387a460beca6e8072cca32a5c911046b8fe24cd
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-05-12 18:05:06 -0300

    Remoção da gti no email

--- lib/app/modules/home/ui/pages/footer/footer.dart
+++ lib/app/modules/home/ui/pages/footer/footer.dart
@@ -130,18 +130,6 @@ class Footer extends StatelessWidget {
                           ),
                         ),
                         TextSpan(
-                          text: 'gti.central@maua.br',
-                          style: AppTextStyles.titleH1
-                              .copyWith(color: Colors.white, fontSize: 12),
-                        ),
-                        TextSpan(
-                          text: ' ou ',
-                          style: AppTextStyles.body.copyWith(
-                            color: Colors.white,
-                            fontSize: 12,
-                          ),
-                        ),
-                        TextSpan(
                           text: 'dev@maua.br',
                           style: AppTextStyles.titleH1
                               .copyWith(color: Colors.white, fontSize: 12),
@@ -335,18 +323,6 @@ class Footer extends StatelessWidget {
                             children: [
                               TextSpan(
                                 text: 'smile@maua.br',
-                                style: AppTextStyles.titleH1.copyWith(
-                                    color: Colors.white, fontSize: 12),
-                              ),
-                              TextSpan(
-                                text: ' ou ',
-                                style: AppTextStyles.body.copyWith(
-                                  color: Colors.white,
-                                  fontSize: 12,
-                                ),
-                              ),
-                              TextSpan(
-                                text: 'gti.central@maua.br',
                                 style: AppTextStyles.titleH1.copyWith(
                                     color: Colors.white, fontSize: 12),
                               ),
