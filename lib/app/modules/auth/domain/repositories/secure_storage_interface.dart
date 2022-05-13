40523f35e940370c4f4808fbc7e5cdd76aa24aef
Gabriel de Godoy Braz <gabriel.godoybz@hotmail.com>
2022-05-12 23:28:51 -0300

    Saving id in Storage

--- lib/app/modules/auth/domain/repositories/secure_storage_interface.dart
+++ lib/app/modules/auth/domain/repositories/secure_storage_interface.dart
@@ -4,10 +4,12 @@ abstract class SecureStorageInterface {
   Future<String?> getAccessToken();
   Future<String?> getRefreshToken();
   Future<void> saveAccessLevel(String accessLevel);
+  Future<void> saveId(String id);
   Future<void> saveName(String name);
   Future<void> saveSocialName(String socialName);
   Future<void> saveCertificateWithSocialName(bool certificateWithSocialName);
   Future<String?> getAccessLevel();
+  Future<String?> getId();
   Future<String?> getName();
   Future<String?> getSocialName();
   Future<bool?> getCertificateWithSocialName();
