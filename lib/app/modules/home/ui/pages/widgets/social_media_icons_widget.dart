a323fc0e497724788a6484340ef8f70e7fa89bd8
Breno <brenoamorim11@gmail.com>
2022-09-14 23:28:11 -0300

    Icones certinhos, com as listas e com os urls funcionando. Todos em laranja

--- lib/app/modules/home/ui/pages/widgets/social_media_icons_widget.dart
+++ lib/app/modules/home/ui/pages/widgets/social_media_icons_widget.dart
@@ -1,110 +1,72 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:fluttericon/entypo_icons.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:fluttericon/font_awesome_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class SocialMediaWidget extends StatelessWidget {
   const SocialMediaWidget({Key? key}) : super(key: key);
 
+  final urls = const <String>[
+    'http://blog.maua.br/?_ga=2.199500717.2083227948.1663106128-1460562002.1628678318&_gl=1%2at8yuqa%2a_ga%2aMTQ2MDU2MjAwMi4xNjI4Njc4MzE4%2a_ga_Q0DH4GG89H%2aMTY2MzEwNjEyOC44LjAuMTY2MzEwNjEyOC42MC4wLjA.',
+    'http://www.youtube.com/infomaua',
+    'https://www.instagram.com/infomaua/',
+    'https://www.linkedin.com/school/479773/admin/updates',
+    'http://www.flickr.com/photos/infomaua',
+    'https://twitter.com/InfoMaua',
+    'https://www.facebook.com/InstitutoMaua',
+  ];
+  final myIcons = const <Widget>[
+    Icon(FontAwesome5.blogger_b, size: 17),
+    Icon(FontAwesome.youtube_play, size: 17),
+    Icon(FontAwesome5.instagram, size: 17),
+    Icon(FontAwesome5.linkedin_in, size: 17),
+    Icon(Entypo.flickr, size: 17),
+    Icon(FontAwesome5.twitter, size: 17),
+    Icon(FontAwesome5.facebook_f, size: 17),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Row(
-        children: [
-          Container(
-            width: 40,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(100)),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(), primary: Colors.red),
-              onPressed: () => launchUrl(
-                Uri.parse('https://www.instagram.com/infomaua/'),
-                mode: LaunchMode.externalApplication,
-              ),
-              child: const Text("I"),
-            ),
-          ),
-          Container(
-            width: 40,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(100)),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(), primary: Colors.red),
-              onPressed: () => launchUrl(
-                Uri.parse(
-                    'http://blog.maua.br/?_ga=2.199500717.2083227948.1663106128-1460562002.1628678318&_gl=1%2at8yuqa%2a_ga%2aMTQ2MDU2MjAwMi4xNjI4Njc4MzE4%2a_ga_Q0DH4GG89H%2aMTY2MzEwNjEyOC44LjAuMTY2MzEwNjEyOC42MC4wLjA.'),
-                mode: LaunchMode.externalApplication,
-              ),
-              child: const Text("B"),
-            ),
-          ),
-          Container(
-            width: 40,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(100)),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(), primary: Colors.red),
-              onPressed: () => launchUrl(
-                Uri.parse(
-                    'https://www.linkedin.com/school/479773/admin/updates'),
-                mode: LaunchMode.externalApplication,
-              ),
-              child: const Text("L"),
-            ),
-          ),
-          Container(
-            width: 40,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(100)),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(), primary: Colors.red),
-              onPressed: () => launchUrl(
-                Uri.parse('http://www.youtube.com/infomaua'),
-                mode: LaunchMode.externalApplication,
-              ),
-              child: const Text("Y"),
-            ),
-          ),
-          Container(
-            width: 40,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(100)),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(), primary: Colors.red),
-              onPressed: () => launchUrl(
-                Uri.parse('http://www.flickr.com/photos/infomaua'),
-                mode: LaunchMode.externalApplication,
-              ),
-              child: const Text("F"),
-            ),
-          ),
-          Container(
-            width: 40,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(100)),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(), primary: Colors.red),
-              onPressed: () => launchUrl(
-                Uri.parse('https://twitter.com/InfoMaua'),
-                mode: LaunchMode.externalApplication,
-              ),
-              child: const Text("T"),
-            ),
-          ),
-          Container(
-            width: 40,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(100)),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: const CircleBorder(), primary: Colors.red),
-              onPressed: () => launchUrl(
-                Uri.parse('https://www.facebook.com/InstitutoMaua'),
-                mode: LaunchMode.externalApplication,
-              ),
-              child: const Text("F"),
-            ),
-          ),
-        ],
+        child: SizedBox(
+      width: 300,
+      height: 30,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 7,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: SocialMediaIconsWidget(
+                  url: urls[index], icon: myIcons[index]),
+            );
+          }),
+    ));
+  }
+}
+
+// ignore: must_be_immutable
+class SocialMediaIconsWidget extends StatelessWidget {
+  SocialMediaIconsWidget({super.key, required this.icon, required this.url});
+
+  late int index;
+  late Widget icon;
+  late String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => launchUrl(
+        Uri.parse(url),
+        mode: LaunchMode.externalApplication,
+      ),
+      child: CircleAvatar(
+        radius: 15,
+        backgroundColor: Colors.deepOrange,
+        child: icon,
       ),
     );
   }
