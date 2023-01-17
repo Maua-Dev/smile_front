a89e90737aa3c3b88189b9d7af0ba49e3f1c09e2
Breno Amorim <brenoamorim11@gmail.com>
2023-01-17 14:45:20 -0300

    useCase ResendConfirmation

---
+++ lib/app/modules/resend-confirmation/usecases/resend_confirmation.dart
@@ -0,0 +1,16 @@
+import '../domain/repository/resend_confirmation_datasource_interface.dart';
+
+abstract class ResendConfirmationInterface {
+  Future<String> call(String username);
+}
+
+class ResendConfirmation implements ResendConfirmationInterface {
+  final ResendConfirmationRepositoryInterface repository;
+
+  ResendConfirmation({required this.repository});
+
+  @override
+  Future<String> call(String username) {
+    return repository.resendConfirmation(username);
+  }
+}
--- test/app/modules/resend-confirmation/presenter/controller/resend_confirmation_controller_test.dart
+++ test/app/modules/resend-confirmation/presenter/controller/resend_confirmation_controller_test.dart
@@ -5,22 +5,26 @@ import 'package:mockito/mockito.dart';
 import 'package:smile_front/app/modules/register/register_module.dart';
 import 'package:smile_front/app/modules/resend-confirmation/domain/repository/resend_confirmation_datasource_interface.dart';
 import 'package:smile_front/app/modules/resend-confirmation/presenter/controller/resend_confirmation_controller.dart';
+import 'package:smile_front/app/modules/resend-confirmation/usecases/resend_confirmation.dart';
 
 import 'resend_confirmation_controller_test.mocks.dart';
 
-@GenerateMocks([ResendConfirmationRepositoryInterface])
+@GenerateMocks(
+    [ResendConfirmationRepositoryInterface, ResendConfirmationInterface])
 void main() {
   initModules([RegisterModule()]);
   ResendConfirmationRepositoryInterface repository =
       MockResendConfirmationRepositoryInterface();
+  ResendConfirmationInterface resendUserConfirmation =
+      MockResendConfirmationInterface();
   late ResendConfirmationController controller;
 
   var cpf = '123.456.789-0';
 
   setUpAll(() async {
     when(repository.resendConfirmation(cpf)).thenAnswer((_) async => '');
-    controller =
-        ResendConfirmationController(resendConfirmationRepository: repository);
+    controller = ResendConfirmationController(
+        resendUserConfirmation: resendUserConfirmation);
   });
 
   test('setError', () {
