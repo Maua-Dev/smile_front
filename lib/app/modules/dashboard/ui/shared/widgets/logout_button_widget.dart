8d76515b26bb97715bd1784c996af11636422751
Enrico Santarelli <enricosantarelli@hotmail.com>
2023-01-20 16:48:36 -0300

    widet_side_bar_e_app_bar

--- lib/app/modules/dashboard/ui/shared/widgets/logout_button_widget.dart
+++ lib/app/modules/dashboard/ui/shared/widgets/logout_button_widget.dart
@@ -20,7 +20,7 @@ class LogoutButtonWidget extends StatelessWidget {
           elevation: MaterialStateProperty.all(20),
           backgroundColor: MaterialStateProperty.all(backgroundColor),
           shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
           )),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
