f6dab541a120f0f5304c77daf7c4394afbae3fd0
Enrico Santarelli <enricosantarelli@hotmail.com>
2023-01-26 13:07:12 -0300

    filter_fix

--- lib/app/modules/dashboard/ui/adm/widgets/filter/filter_card_widget.dart
+++ lib/app/modules/dashboard/ui/adm/widgets/filter/filter_card_widget.dart
@@ -1,13 +1,22 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:smile_front/app/modules/dashboard/domain/infra/activity_enum.dart';
 import 'package:smile_front/app/shared/themes/app_colors.dart';
 import 'package:smile_front/app/shared/themes/app_text_styles.dart';
 
-class FilterCardWidget extends StatelessWidget {
+class FilterCardWidget extends StatefulWidget {
   const FilterCardWidget({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<FilterCardWidget> createState() => _FilterCardWidgetState();
+}
+
+class _FilterCardWidgetState extends State<FilterCardWidget> {
+  String _dateString = '';
+  String _timeString = '';
 
   @override
   Widget build(BuildContext context) {
@@ -27,99 +36,121 @@ class FilterCardWidget extends StatelessWidget {
         height: 47,
         child:
             Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          DropdownButtonFormField(
-            isExpanded: true,
-            decoration: InputDecoration(
-              isDense: true,
-              isCollapsed: false,
-              hintText: 'Atividades',
-              fillColor: AppColors.white,
-              constraints: const BoxConstraints(maxHeight: 47, maxWidth: 227),
-              filled: true,
-              hintStyle: AppTextStyles.body.copyWith(fontSize: 30),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          Container(
+            width: 227,
+            height: 47,
+            decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.circular(10)),
+            child: DropdownButtonFormField(
+              iconSize: 24,
+              isExpanded: true,
+              decoration: InputDecoration(
+                isDense: true,
+                isCollapsed: false,
+                hintText: 'Atividades',
+                fillColor: AppColors.white,
+                filled: true,
+                hintStyle: AppTextStyles.body.copyWith(fontSize: 30),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+              ),
+              items: ActivityEnum.values.map((ActivityEnum activityEnum) {
+                return DropdownMenuItem<ActivityEnum>(
+                    value: activityEnum,
+                    child: Text(activityEnum.name.toString(),
+                        style: AppTextStyles.body.copyWith(fontSize: 20)));
+              }).toList(),
+              onChanged: (value) {},
             ),
-            items: ActivityEnum.values.map((ActivityEnum activityEnum) {
-              return DropdownMenuItem<ActivityEnum>(
-                  value: activityEnum,
-                  child: Text(activityEnum.name.toString(),
-                      style: AppTextStyles.body.copyWith(fontSize: 20)));
-            }).toList(),
-            onChanged: (value) {},
           ),
-          InkWell(
-            child: Container(
-              width: 227,
-              height: 47,
-              decoration: BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: BorderRadius.circular(10)),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(12, 0, 12, 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text('Data',
-                        style: AppTextStyles.body.copyWith(fontSize: 30)),
-                    const Icon(
-                      Icons.arrow_drop_down,
-                      size: 24,
+          Container(
+            width: 227,
+            height: 47,
+            decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(12, 0, 12, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(_dateString == '' ? 'Data' : _dateString,
+                      style: AppTextStyles.body
+                          .copyWith(fontSize: _dateString == '' ? 30 : 20)),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 3),
+                    child: IconButton(
+                      onPressed: () {
+                        showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(2022),
+                                lastDate: DateTime(2222))
+                            .then((date) {
+                          setState(() {
+                            _dateString =
+                                DateFormat('dd/MM/yyyy').format(date!);
+                          });
+                        });
+                      },
+                      highlightColor: AppColors.white,
+                      hoverColor: AppColors.white,
+                      icon: const Icon(Icons.arrow_drop_down),
                       color: Colors.black,
+                      iconSize: 24,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-            onTap: () {
-              showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2022),
-                  lastDate: DateTime(2222));
-            },
           ),
-          InkWell(
-            child: Container(
-              width: 227,
-              height: 47,
-              decoration: BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: BorderRadius.circular(10)),
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(12, 0, 12, 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text('Hora',
-                        style: AppTextStyles.body.copyWith(fontSize: 30)),
-                    const Icon(
-                      Icons.arrow_drop_down,
-                      size: 24,
-                      color: Colors.black,
-                    ),
-                  ],
-                ),
+          Container(
+            width: 227,
+            height: 47,
+            decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(12, 0, 12, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(_timeString == '' ? 'Hora' : _timeString,
+                      style: AppTextStyles.body
+                          .copyWith(fontSize: _timeString == '' ? 30 : 20)),
+                  IconButton(
+                    onPressed: () {
+                      showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay.now(),
+                          builder: (BuildContext context, Widget? child) {
+                            return Theme(
+                              data: ThemeData.light().copyWith(
+                                primaryColor: AppColors.brandingBlue,
+                                colorScheme: ColorScheme.light(
+                                    primary: AppColors.brandingBlue),
+                              ),
+                              child: child!,
+                            );
+                          }).then((time) {
+                        setState(() {
+                          _timeString = time!.format(context);
+                        });
+                      });
+                    },
+                    highlightColor: AppColors.white,
+                    hoverColor: AppColors.white,
+                    icon: const Icon(Icons.arrow_drop_down),
+                    color: Colors.black,
+                    iconSize: 24,
+                  ),
+                ],
               ),
             ),
-            onTap: () {
-              showTimePicker(
-                  context: context,
-                  initialTime: TimeOfDay.now(),
-                  builder: (BuildContext context, Widget? child) {
-                    return Theme(
-                      data: ThemeData.light().copyWith(
-                        primaryColor: AppColors.brandingBlue,
-                        colorScheme:
-                            ColorScheme.light(primary: AppColors.brandingBlue),
-                      ),
-                      child: child!,
-                    );
-                  });
-            },
-          )
+          ),
         ]));
   }
 }
