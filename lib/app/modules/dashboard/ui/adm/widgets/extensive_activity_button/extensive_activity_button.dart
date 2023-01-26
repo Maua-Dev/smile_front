0099aa84519bdb546e92520cb4ddf9b12e149e1c
Breno Amorim <brenoamorim11@gmail.com>
2023-01-26 16:01:15 -0300

    Botão de Atividade Extensiva Terminado Funcionando

--- lib/app/modules/dashboard/ui/adm/widgets/extensive_activity_button/extensive_activity_button.dart
+++ lib/app/modules/dashboard/ui/adm/widgets/extensive_activity_button/extensive_activity_button.dart
@@ -12,30 +12,43 @@ class ExtensiveActivityCheck extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.only(top: 24.0, left: 24),
-        child: Container(
-          height: 50,
-          width: 125,
-          decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  spreadRadius: 1,
-                  blurRadius: 3,
-                  offset: const Offset(5, 5), // changes position of shadow
-                ),
-              ],
-              color: AppColors.white,
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(width: 1, color: AppColors.brandingBlue)),
-          child: MouseRegion(
-            cursor: SystemMouseCursors.click,
-            child: GestureDetector(
-              onTap: onChanged,
-              child: Icon(isExtensive ? Icons.star : Icons.star_border,
-                  color: AppColors.brandingOrange),
+      padding: const EdgeInsets.only(left: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(bottom: 4.0, left: 4),
+            child: Text(
+              "Extensiva",
+              style: TextStyle(fontSize: 18),
             ),
           ),
-        ));
+          Container(
+            height: 50,
+            width: 125,
+            decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                    offset: const Offset(5, 5), // changes position of shadow
+                  ),
+                ],
+                color: AppColors.white,
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(width: 1, color: AppColors.brandingBlue)),
+            child: MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: GestureDetector(
+                onTap: onChanged,
+                child: Icon(isExtensive ? Icons.star : Icons.star_border,
+                    color: AppColors.brandingOrange),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
