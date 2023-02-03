2f862e0eeabac0b5f529cc30c66a090e8c75809b
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-02-02 22:37:29 -0300

    Update all_activities_user_dashboard_controller_test.dart

--- test/app/modules/dashboard/presenter/controller/user/all_activities_user_dashboard_controller_test.dart
+++ test/app/modules/dashboard/presenter/controller/user/all_activities_user_dashboard_controller_test.dart
@@ -102,7 +102,7 @@ void main() {
           company: 'Oracle',
         ),
       ],
-      startDate: DateTime.parse('2022-05-16 13:00'),
+      startDate: DateTime.parse('2022-05-17 13:00'),
       totalSlots: 20,
       duration: 120,
       place: 'H244',
@@ -136,7 +136,177 @@ void main() {
           company: 'Oracle',
         ),
       ],
-      startDate: DateTime.parse('2022-05-16 13:00'),
+      startDate: DateTime.parse('2022-05-18 13:00'),
+      totalSlots: 20,
+      duration: 120,
+      place: 'H244',
+      link: 'https://www.google.com.br',
+      acceptingNewEnrollments: false,
+      isExtensive: false,
+      takenSlots: 0,
+      responsibleProfessors: [],
+    ),
+    ActivityModel(
+      activityCode: 'C01',
+      type: ActivityEnum.COURSE,
+      title:
+          'Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01',
+      description:
+          'Teste de atividade mock Teste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mock',
+      speakers: [
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+      ],
+      startDate: DateTime.parse('2022-05-19 13:00'),
+      totalSlots: 20,
+      duration: 120,
+      place: 'H244',
+      link: 'https://www.google.com.br',
+      acceptingNewEnrollments: false,
+      isExtensive: false,
+      takenSlots: 0,
+      responsibleProfessors: [],
+    ),
+    ActivityModel(
+      activityCode: 'C01',
+      type: ActivityEnum.COURSE,
+      title:
+          'Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01',
+      description:
+          'Teste de atividade mock Teste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mock',
+      speakers: [
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+      ],
+      startDate: DateTime.parse('2022-05-20 13:00'),
+      totalSlots: 20,
+      duration: 120,
+      place: 'H244',
+      link: 'https://www.google.com.br',
+      acceptingNewEnrollments: false,
+      isExtensive: false,
+      takenSlots: 0,
+      responsibleProfessors: [],
+    ),
+    ActivityModel(
+      activityCode: 'C01',
+      type: ActivityEnum.COURSE,
+      title:
+          'Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01',
+      description:
+          'Teste de atividade mock Teste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mock',
+      speakers: [
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+      ],
+      startDate: DateTime.parse('2022-05-21 13:00'),
+      totalSlots: 20,
+      duration: 120,
+      place: 'H244',
+      link: 'https://www.google.com.br',
+      acceptingNewEnrollments: false,
+      isExtensive: false,
+      takenSlots: 0,
+      responsibleProfessors: [],
+    ),
+    ActivityModel(
+      activityCode: 'C01',
+      type: ActivityEnum.COURSE,
+      title:
+          'Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01',
+      description:
+          'Teste de atividade mock Teste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mock',
+      speakers: [
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+      ],
+      startDate: DateTime.parse('2022-05-22 13:00'),
+      totalSlots: 20,
+      duration: 120,
+      place: 'H244',
+      link: 'https://www.google.com.br',
+      acceptingNewEnrollments: false,
+      isExtensive: false,
+      takenSlots: 0,
+      responsibleProfessors: [],
+    ),
+    ActivityModel(
+      activityCode: 'C01',
+      type: ActivityEnum.COURSE,
+      title:
+          'Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01Atividade 01',
+      description:
+          'Teste de atividade mock Teste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mockTeste de atividade mock',
+      speakers: [
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+        SpeakerActivityModel(
+          name: 'Gabriel Godoy',
+          bio: 'Caros participantes, este é um teste, aproveitem a atividade',
+          company: 'Oracle',
+        ),
+      ],
+      startDate: DateTime.parse('2022-05-23 13:00'),
       totalSlots: 20,
       duration: 120,
       place: 'H244',
@@ -197,7 +367,7 @@ void main() {
   });
 
   test('saturdayActivitiesList', () {
-    expect(controller.saturdayActivitiesList.isNotEmpty, false);
+    expect(controller.saturdayActivitiesList.isNotEmpty, true);
   });
 
   test('toggleFilterActivityChipIndex', () {
@@ -207,7 +377,7 @@ void main() {
 
   test('getActivitiesByType', () {
     controller.getActivitiesByType(ActivityEnum.COURSE);
-    expect(controller.activitiesList.length, 5);
+    expect(controller.activitiesList.length, mockActivities.length);
   });
 
   test('logout', () {
