b370bbc1ac713c00aceaf3603fd1c9b1f7b3307f
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-03-06 23:11:26 -0300

    Revert "Merge branch 'develop' of https://github.com/Maua-Dev/smile_front into develop"
    This reverts commit 953a4c6e9a32fcc69acb9d28eae0ab99d8b8943a, reversing
    changes made to b74bad28ae5963b657d92af30fe135748ad31597.

--- lib/app/modules/dashboard/presenter/controllers/user/user_dashboard_controller.dart
+++ lib/app/modules/dashboard/presenter/controllers/user/user_dashboard_controller.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:fl_country_code_picker/fl_country_code_picker.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:mobx/mobx.dart';
 import 'package:smile_front/app/modules/auth/domain/repositories/secure_storage_interface.dart';
 import 'package:smile_front/app/modules/dashboard/domain/infra/activity_enum.dart';
@@ -75,22 +74,6 @@ abstract class UserDashboardControllerBase with Store {
     typeFilter = value;
     typeOnScreen = value.name;
     setAllFilters();
-  }
-
-  Future<void> subscribeUserActivity(String activityCode) async {
-    setIsLoading(true);
-    await enrollmentController.subscribeActivity(activityCode);
-    await getUserSubscribedActivities();
-    setIsLoading(false);
-    Modular.to.pop();
-  }
-
-  Future<void> unsubscribeUserActivity(String activityCode) async {
-    setIsLoading(true);
-    await enrollmentController.unsubscribeActivity(activityCode);
-    await getUserSubscribedActivities();
-    setIsLoading(false);
-    Modular.to.pop();
   }
 
   @observable
