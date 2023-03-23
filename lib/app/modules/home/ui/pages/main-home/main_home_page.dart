e06fb23e0c681ebeeae8712e2bab5333bb11ad90
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-03-23 20:48:20 -0300

    new home page design

--- lib/app/modules/home/ui/pages/main-home/main_home_page.dart
+++ lib/app/modules/home/ui/pages/main-home/main_home_page.dart
@@ -8,6 +8,7 @@ import '../../../../../shared/themes/app_text_styles.dart';
 import '../../../../../shared/utils/s3_assets_url.dart';
 import '../../../../../shared/utils/screen_helper.dart';
 import '../../../../../shared/widgets/custom_elevated_button_widget.dart';
+import '../widgets/header/h1_header_text_widget.dart';
 
 class MainHomePage extends StatefulWidget {
   const MainHomePage({Key? key}) : super(key: key);
@@ -21,211 +22,123 @@ class _MainHomePageState extends State<MainHomePage> {
   Widget build(BuildContext context) {
     if (Screen.width(context) > cellphoneSize) {
       return SizedBox(
-        height: Screen.height(context),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(
-                child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: CachedNetworkImageProvider(
-                            '${previousEditionsBaseUrl}main_home.png'),
-                        fit: BoxFit.cover,
-                      ),
+        height: Screen.height(context) - 56,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 64),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    H1HeaderTextWidget(
+                      padding: const EdgeInsets.only(bottom: 32),
+                      title: S.of(context).homePageTitle,
                     ),
-                    child: Padding(
-                        padding: EdgeInsets.only(
-                          right: Screen.width(context) < tabletSize ? 24 : 32,
-                          bottom: 55,
-                        ),
-                        child: Align(
-                          alignment: Alignment.centerRight,
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  width: Screen.width(context) < cellphoneSize
-                                      ? 350
-                                      : Screen.width(context) * 0.5 <
-                                              cellphoneSize
-                                          ? Screen.width(context) * 0.45
-                                          : 650,
-                                  decoration: BoxDecoration(
-                                      color: AppColors.brandingBlue,
-                                      borderRadius: const BorderRadius.all(
-                                          Radius.circular(10))),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(24),
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                            S
-                                                .of(context)
-                                                .homePageTitleSelection('one'),
-                                            style: AppTextStyles.titleH1
-                                                .copyWith(
-                                                    color: Colors.white,
-                                                    fontSize:
-                                                        Screen.width(context) <
-                                                                tabletSize
-                                                            ? 24
-                                                            : 32)),
-                                        Text(
-                                            S
-                                                .of(context)
-                                                .homePageTitleSelection('two'),
-                                            style: AppTextStyles.titleH1
-                                                .copyWith(
-                                                    color: Colors.white,
-                                                    fontSize:
-                                                        Screen.width(context) <
-                                                                tabletSize
-                                                            ? 24
-                                                            : 32)),
-                                        const SizedBox(
-                                          height: 16,
-                                        ),
-                                        SizedBox(
-                                          width:
-                                              Screen.width(context) < tabletSize
-                                                  ? Screen.width(context) <
-                                                          cellphoneSize
-                                                      ? 300
-                                                      : 580
-                                                  : 820,
-                                          //580 e 820
-                                          child: Text(
-                                            S.of(context).homePageSubtitle,
-                                            style: AppTextStyles.body.copyWith(
-                                                color: Colors.white,
-                                                fontSize: Screen.width(
-                                                            context) <
-                                                        tabletSize
-                                                    ? Screen.width(context) <
-                                                            cellphoneSize
-                                                        ? 16
-                                                        : 18
-                                                    : 25),
-                                            textAlign: TextAlign.justify,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: Screen.width(context) < tabletSize
-                                      ? 9
-                                      : 16,
-                                ),
-                                CustomElevatedButtonWidget(
-                                  title: S.of(context).smileDateWithYear,
-                                  textStyle: AppTextStyles.buttonBold.copyWith(
-                                      color: Colors.white,
-                                      fontSize:
-                                          Screen.width(context) < tabletSize
-                                              ? Screen.width(context) <
-                                                      cellphoneSize
-                                                  ? 20
-                                                  : 24
-                                              : 40),
-                                  widthSize: Screen.width(context) < tabletSize
-                                      ? 320
-                                      : 500,
-                                  heightSize:
-                                      Screen.width(context) < cellphoneSize
-                                          ? 40
-                                          : 50,
-                                  borderRadius: 40,
-                                  backgroundColor: AppColors.brandingBlue,
-                                  isClickable: false,
-                                  onPressed: null,
-                                ),
-                                SizedBox(
-                                    height: Screen.width(context) < tabletSize
-                                        ? 9
-                                        : 16),
-                                CustomElevatedButtonWidget(
-                                  title: S.of(context).knowMore,
-                                  textStyle: AppTextStyles.buttonBold.copyWith(
-                                      color: Colors.white,
-                                      fontSize:
-                                          Screen.width(context) < tabletSize
-                                              ? Screen.width(context) <
-                                                      cellphoneSize
-                                                  ? 20
-                                                  : 24
-                                              : 40),
-                                  widthSize: Screen.width(context) < tabletSize
-                                      ? Screen.width(context) < cellphoneSize
-                                          ? 200
-                                          : 240
-                                      : 340,
-                                  heightSize:
-                                      Screen.width(context) < cellphoneSize
-                                          ? 40
-                                          : 50,
-                                  borderRadius: 40,
-                                  backgroundColor: AppColors.brandingOrange,
-                                  onPressed: () {
-                                    Modular.to.navigate('/home/home-more-info');
-                                  },
-                                )
-                              ]),
-                        ))))
-          ],
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      S.of(context).homePageSubtitle,
+                      style: AppTextStyles.body.copyWith(fontSize: 25),
+                      textAlign: TextAlign.justify,
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    CustomElevatedButtonWidget(
+                      title: S.of(context).smileDateWithYear,
+                      textStyle: AppTextStyles.buttonBold
+                          .copyWith(color: Colors.white, fontSize: 24),
+                      widthSize: 400,
+                      heightSize: 50,
+                      borderRadius: 40,
+                      backgroundColor: AppColors.brandingBlue,
+                      isClickable: false,
+                      onPressed: null,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    CustomElevatedButtonWidget(
+                      title: S.of(context).knowMore,
+                      textStyle: AppTextStyles.buttonBold
+                          .copyWith(color: Colors.white, fontSize: 24),
+                      widthSize: 400,
+                      heightSize: 50,
+                      borderRadius: 40,
+                      backgroundColor: AppColors.brandingOrange,
+                      onPressed: () {
+                        Modular.to.navigate('/home/home-more-info');
+                      },
+                    )
+                  ],
+                ),
+              ),
+              Flexible(
+                  child: Center(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 104, horizontal: 64),
+                  child: Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: CachedNetworkImageProvider(
+                                  '${previousEditionsBaseUrl}main_home.png')))),
+                ),
+              ))
+            ],
+          ),
         ),
       );
     } else {
-      return Column(
-        children: [
-          Image(
-            image: CachedNetworkImageProvider(
-                '${previousEditionsBaseUrl}main_home.png'),
-            fit: BoxFit.fitWidth,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8),
-            child: Center(
+      return SizedBox(
+        child: Column(
+          children: [
+            H1HeaderTextWidget(
+              title: S.of(context).homePageTitle,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Text(
-                S.of(context).homePageTitle,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
+                S.of(context).homePageSubtitle,
+                style: const TextStyle(fontSize: 14),
+                textAlign: TextAlign.justify,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              S.of(context).homePageSubtitle,
-              style: const TextStyle(fontSize: 14),
-              textAlign: TextAlign.justify,
+            const SizedBox(
+              height: 16,
             ),
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          Container(
-            width: 200,
-            height: 40,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: AppColors.brandingOrange,
-              borderRadius: const BorderRadius.all(Radius.circular(20)),
+            Image(
+              image: CachedNetworkImageProvider(
+                  '${previousEditionsBaseUrl}main_home.png'),
+              fit: BoxFit.fitWidth,
             ),
-            child: Text(
-              S.of(context).smileDate,
-              textAlign: TextAlign.center,
-              style: AppTextStyles.buttonBold.copyWith(fontSize: 16),
+            const SizedBox(
+              height: 16,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: MouseRegion(
+            Container(
+              width: 200,
+              height: 40,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: AppColors.brandingOrange,
+                borderRadius: const BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Text(
+                S.of(context).smileDate,
+                textAlign: TextAlign.center,
+                style: AppTextStyles.buttonBold.copyWith(fontSize: 16),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            MouseRegion(
               cursor: SystemMouseCursors.click,
               child: GestureDetector(
                 onTap: () {
@@ -247,8 +160,8 @@ class _MainHomePageState extends State<MainHomePage> {
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
     }
   }
--- lib/app/modules/home/ui/pages/widgets/header/h1_header_text_widget.dart
+++ lib/app/modules/home/ui/pages/widgets/header/h1_header_text_widget.dart
@@ -5,12 +5,13 @@ import '../../../../../../shared/themes/app_text_styles.dart';
 
 class H1HeaderTextWidget extends StatelessWidget {
   final String title;
-  const H1HeaderTextWidget({super.key, required this.title});
+  final EdgeInsets? padding;
+  const H1HeaderTextWidget({super.key, required this.title, this.padding});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 32),
+      padding: padding ?? const EdgeInsets.symmetric(vertical: 32),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
