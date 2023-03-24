cae9c8d0e34dbf39094309c0a79e6333c83ff669
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2023-03-24 14:36:19 -0300

    novos faqs

--- lib/app/modules/dashboard/utils/mocks/faq_mock.dart
+++ lib/app/modules/dashboard/utils/mocks/faq_mock.dart
@@ -2,80 +2,56 @@ import '../../domain/entities/faq.dart';
 
 var faqMock = <Faq>[
   Faq(
-      id: 0,
       question: 'Fiz o meu login e esqueci a senha. O que devo fazer?',
       response:
           'Existe uma opção na tela de "Login" chamada "Esqueci minha senha", onde você poderá alterá-la seguindo um passo-a-passo.'),
   Faq(
-      id: 1,
       question: 'Como validar a presença nas minhas atividades?',
       response:
-          'Será disponibilizado durante a atividade um Formulário para preenchimento do participante que validará as suas horas na atividade.'),
+          'Clicando na atividade, você será direcionado para a tela de mais informações, onde encontrará um botão para validação de presença. Ao seleciona-lo, você deve preencher um token com 6 dígitos, que será fornecido pelo professor responsável pela atividade. Lembrando que o botão de validação só estará disponível caso você esteja inscrito na atividade. Qualquer problema com o token informe o professor responsável imediatamente.'),
   Faq(
-      id: 2,
-      question: 'Em que local no site eu vejo as atividades da SMILE?',
+      question: 'Em que local do site eu vejo as atividades da SMILE?',
       response:
           'Clicando no ícone central da barra de navegação você poderá visualizar todas as atividades do evento. Se você for estudante do IMT poderá também acessar pelo Open LMS da SMILE: https://imt.myopenlms.net/course/view.php?id=1353'),
   Faq(
-      id: 3,
       question: 'Na atividade presencial, onde vejo a sala?',
       response:
           'Ao clicar na atividade em que você se inscreveu, você poderá ver todas as informações disponíveis sobre aquela atividade, inclusive a sala.'),
   Faq(
-      id: 4,
       question: 'Nas visitas técnicas posso estar de bermuda?',
       response:
           'As Visitas Técnicas tem algumas orientações importantes! Assim ANTES de você fazer a sua inscrição LEIA com ATENÇÃO as informações necessárias que estão no site da SMILE. A recomendação padrão para você participar de uma visita é estar vestido de calça comprida e sapato de couro. Bermudas não serão permitidas.'),
   Faq(
-      id: 5,
-      question:
-          'As gravações das atividades on-line estarão disponíveis? Onde e quando?',
+      question: 'Terão gravações das atividades online esse ano?',
       response:
-          'As gravações estarão disponíveis a partir do dia 30.05. Se você for do IMT poderá fazer o download no ambiente Open LMS da SMILE e caso você seja um participante externo poderá solicitar pelo e-mail dev@maua.br'),
+          'Na SMILE 2023, não haverão gravações das atividades realizadas online.'),
   Faq(
-      id: 6,
       question: 'Como cancelar minha inscrição em uma atividade?',
       response:
           'Ao clicar na atividade que você quer cancelar haverá um botão vermelho "Desinscrever-se", basta clicar nele e confirmar.'),
   Faq(
-      id: 7,
-      question: 'Como realizo acesso ao Zoom para assistir a palestra?',
+      question: 'Como assistir minhas atividades online?',
       response:
-          'Ao clicar na atividade em que você se inscreveu haverá um campo "Link" sublinhado e clicável de "Zoom" que irá te redirecionar para a reunião.'),
+          'Clicando na atividade em questão, você terá acesso a mais informações sobre ela. Dentre as informações, estará o link para a atividade.'),
   Faq(
-      id: 8,
-      question: 'Inseri meu e-mail errado. Como pedir alteração?',
-      response: 'Envie um e-mail para dev@maua.br ou ligue para 4239-3169.'),
-  Faq(
-      id: 9,
       question: 'Como emitir o meu Certificado?',
       response:
-          'O Certificado será emitido por você acessando o site da SMILE.'),
+          'Os certificados serão disponibilizados automáticamente no seu perfil 10 dias após o evento. Caso ocorra algum erro, entre em contato com nosso suporte: antendimento.smile@maua.br.'),
   Faq(
-      id: 10,
       question: 'Como alterar meu nome/nome social para a certificação?',
       response:
-          'Basta navegar até a página de perfil e clicar no icone de lápis ao lado de seu nome.'),
+          'Ao entrar na aba de perfil, você poderá alterar alguns de seus dados clicando no lápis no canto superior direito. A data limite para troca de nomes antes da emissão do certificado é de 5 dias após o término do evento (02/06/2023).'),
   Faq(
-      id: 11,
       question:
           'Digitei meu nome com uma letra errada no meu Certificado. Até que data posso alterar?',
       response:
-          'O nome incluído no sistema será o que utilizaremos para fins de emissão de certificado. Qualquer alteração no seu cadastro poderá ser feita até o dia 23.04, sob pena do certificado ser emitido com os dados fornecidos por você. Fique atento!'),
+          'A data limite para solicitações de correções nos certificados é até dia (13/06/2023), sendo a data limite para a entrega das correções até dia (19/06/2023).'),
   Faq(
-      id: 12,
       question: 'A partir de quando posso emitir meu certificado?',
       response:
           'Os Certificados estarão disponíveis para download no site a partir de 30 de maio de 2023.'),
   Faq(
-      id: 13,
-      question:
-          'Quais são os links nas redes sociais para compartilhar o evento com amigos?',
-      response:
-          'Visite o site da SMILE: smile.maua.br e você poderá encontrar toda a programação. Você também poderá acompanhar as informações pelo nosso Instagram @infomaua'),
-  Faq(
-      id: 14,
       question: 'Em caso de dúvidas, como faço para entrar em contato?',
       response:
-          'Envie um e-mail para dev@maua.br. Caso queira falar conosco, entre em contato nos números: 4239-3002, 4239-3014, 4239-3441, 4239-3442 ou 4239-3411.'),
+          'Para mais informações sobre o evento entre em contato com nosso suporte: antendimento.smile@maua.br'),
 ];
