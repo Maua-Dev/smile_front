c809c0f7a76b6eebca9abde076bcde6c9460e3a0
Breno Amorim <brenoamorim11@gmail.com>
2023-04-20 17:12:37 -0300

    entities and patro

--- lib/app/modules/home/domain/infra/maua_entities_enum.dart
+++ lib/app/modules/home/domain/infra/maua_entities_enum.dart
@@ -7,18 +7,18 @@ enum MauaEntitiesEnum {
   ca,
   diversidade,
   enactus,
-  esports,
+  //esports,
   inova,
   mauajr,
   mauasocial,
-  mbc,
+  //mbc,
   nae,
-  nine,
+  //nine,
   finance,
-  baja,
+  //baja,
   ecomaua,
-  mauaracing2,
-  mauaracing
+  // mauaracing2,
+  // mauaracing
 }
 
 extension MauaEntitiesEnumExtension on MauaEntitiesEnum {
@@ -40,32 +40,32 @@ extension MauaEntitiesEnumExtension on MauaEntitiesEnum {
         return '${s3AssetsBaseUrl}logo_entidades_diversidade.png';
       case MauaEntitiesEnum.enactus:
         return '${s3AssetsBaseUrl}logo_entidades_enactus.png';
-      case MauaEntitiesEnum.esports:
-        return '${s3AssetsBaseUrl}logo_entidades_esports.png';
+      // case MauaEntitiesEnum.esports:
+      //   return '${s3AssetsBaseUrl}logo_entidades_esports.png';
       case MauaEntitiesEnum.inova:
         return '${s3AssetsBaseUrl}logo_entidades_inova.png';
       case MauaEntitiesEnum.mauajr:
         return '${s3AssetsBaseUrl}logo_entidades_mauajr.png';
       case MauaEntitiesEnum.mauasocial:
         return '${s3AssetsBaseUrl}logo_entidades_social.png';
-      case MauaEntitiesEnum.mbc:
-        return '${s3AssetsBaseUrl}logo_entidades_business.png';
+      // case MauaEntitiesEnum.mbc:
+      //   return '${s3AssetsBaseUrl}logo_entidades_business.png';
       case MauaEntitiesEnum.nae:
         return '${s3AssetsBaseUrl}logo_entidades_nae.png';
       case MauaEntitiesEnum.dev:
         return '${s3AssetsBaseUrl}logo_entidades_dev.png';
-      case MauaEntitiesEnum.nine:
-        return '${s3AssetsBaseUrl}logo_entidades_nine.png';
+      // case MauaEntitiesEnum.nine:
+      //   return '${s3AssetsBaseUrl}logo_entidades_nine.png';
       case MauaEntitiesEnum.finance:
-        return '${s3AssetsBaseUrl}logo_entidades_finance.png';
-      case MauaEntitiesEnum.baja:
+        //   return '${s3AssetsBaseUrl}logo_entidades_finance.png';
+        // case MauaEntitiesEnum.baja:
         return '${s3AssetsBaseUrl}logo_entidades_baja.png';
       case MauaEntitiesEnum.ecomaua:
-        return '${s3AssetsBaseUrl}logo_entidades_eco.png';
-      case MauaEntitiesEnum.mauaracing2:
+        //   return '${s3AssetsBaseUrl}logo_entidades_eco.png';
+        // case MauaEntitiesEnum.mauaracing2:
         return '${s3AssetsBaseUrl}logo_entidades_mauah2.png';
-      case MauaEntitiesEnum.mauaracing:
-        return '${s3AssetsBaseUrl}logo_entidades_racing.png';
+      // case MauaEntitiesEnum.mauaracing:
+      //   return '${s3AssetsBaseUrl}logo_entidades_racing.png';
     }
   }
 }
--- lib/app/modules/home/ui/pages/sponsors-home/sponsors_home_page.dart
+++ lib/app/modules/home/ui/pages/sponsors-home/sponsors_home_page.dart
@@ -334,26 +334,71 @@ class CompanySponsor extends StatelessWidget {
     return SizedBox(
       width: MediaQuery.of(context).size.width / 1.3,
       child: Center(
-        child: Wrap(
-          spacing: 48,
-          runSpacing: 25,
+        child: Column(
           children: [
-            SponsorsWidget(link: mauaAzulLogoUrl, color: Colors.white),
-            SponsorsWidget(link: devLogoUrl, color: Colors.white),
-            SponsorsWidget(link: patrocinadorGCPLogoUrl, color: Colors.white),
-            SponsorsWidget(link: patrocinadorNubeLogoUrl, color: Colors.white),
+            Wrap(
+              alignment: WrapAlignment.center,
+              spacing: 48,
+              runSpacing: 25,
+              children: [
+                SponsorsWidget(
+                  link: mauaAzulLogoUrl,
+                  color: Colors.white,
+                  size: 0.8,
+                ),
+                SponsorsWidget(
+                    link: devLogoUrl, color: Colors.white, size: 0.8),
+              ],
+            ),
+            Wrap(
+              spacing: 48,
+              runSpacing: 25,
+              children: [
+                SponsorsWidget(
+                    link: patrocinadorGCPLogoUrl,
+                    color: Colors.white,
+                    size: 0.7),
+                SponsorsWidget(
+                    link: patrocinadorCSNLogoUrl,
+                    color: Colors.white,
+                    size: 0.7),
+                SponsorsWidget(
+                    link: patrocinadorApplusLogoUrl,
+                    color: Colors.white,
+                    size: 0.7),
+                SponsorsWidget(
+                    link: patrocinadorOsborneLogoUrl,
+                    color: Colors.white,
+                    size: 0.7),
+                SponsorsWidget(
+                    link: patrocinadorAlstomLogoUrl,
+                    color: Colors.white,
+                    size: 0.7),
+                SponsorsWidget(
+                    link: patrocinadorBungeLogoUrl,
+                    color: Colors.white,
+                    size: 0.8),
+              ],
+            ),
+            Wrap(
+              alignment: WrapAlignment.center,
+              spacing: 48,
+              runSpacing: 25,
+              children: [
+                SponsorsWidget(
+                    link: patrocinadorNubeLogoUrl,
+                    color: Colors.white,
+                    size: 0.5),
+                SponsorsWidget(
+                    link: patrocinadorVendraminiLogoUrl,
+                    color: Colors.white,
+                    size: 0.5),
+              ],
+            ),
             SponsorsWidget(
-                link: patrocinadorVendraminiLogoUrl, color: Colors.white),
-            SponsorsWidget(link: patrocinadorCSNLogoUrl, color: Colors.white),
-            SponsorsWidget(
-                link: patrocinadorApplusLogoUrl, color: Colors.white),
-            SponsorsWidget(
-                link: patrocinadorOsborneLogoUrl, color: Colors.white),
-            SponsorsWidget(
-                link: patrocinadorAlstomLogoUrl, color: Colors.white),
-            SponsorsWidget(link: patrocinadorBungeLogoUrl, color: Colors.white),
-            SponsorsWidget(
-                link: patrocinadorCengageLogoUrl, color: Colors.white),
+                link: patrocinadorCengageLogoUrl,
+                color: Colors.white,
+                size: 0.8),
           ],
         ),
       ),
