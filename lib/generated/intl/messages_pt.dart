dbf0575bebbc64f8fd9a27c0e931f0faaa881673
EnricoSantarelli <63019527+EnricoSantarelli@users.noreply.github.com>
2023-05-08 07:11:38 -0300

    email fix

--- lib/generated/intl/messages_pt.dart
+++ lib/generated/intl/messages_pt.dart
@@ -126,7 +126,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static String m10(title) => "${Intl.select(title, {
             'normalPart': 'Em caso de erro ao se logar, envie um e-mail para: ',
-            'boldPart': 'atendimento.smile@maua.com',
+            'boldPart': 'atendimento.smile@maua.br',
             'other': '',
           })}";
 
